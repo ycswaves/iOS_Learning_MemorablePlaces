5017a53d978aac29c4ceef54ce4b8d37f55c2a1d
YiChenshu <ycswaves@gmail.com>
2015-04-05 15:08:45 +0800

    1st commit

--- Memorable Places/ViewController.swift
+++ Memorable Places/ViewController.swift
@@ -7,12 +7,76 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    var manager: CLLocationManager!
+    
+    @IBOutlet weak var map: MKMapView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        manager = CLLocationManager()
+        manager.delegate = self
+        manager.desiredAccuracy = kCLLocationAccuracyBest
+        manager.requestWhenInUseAuthorization()
+        manager.startUpdatingLocation()
+        
+        var uiLongPress = UILongPressGestureRecognizer(target: self, action: "action:")
+        uiLongPress.minimumPressDuration = 2.0
+        map.addGestureRecognizer(uiLongPress)
+        
+    }
+    
+    func action(gestureRec: UIGestureRecognizer) {
+        if gestureRec.state == UIGestureRecognizerState.Began { //1st indication of a particular long press
+            var touchPt = gestureRec.locationInView(self.map)
+            var newCoord = self.map.convertPoint(touchPt, toCoordinateFromView: self.map)
+            var location = CLLocation(latitude: newCoord.latitude, longitude: newCoord.longitude)
+            
+            CLGeocoder().reverseGeocodeLocation(location, completionHandler: { (places, error) -> Void in
+                var annote = ""
+                if error == nil {
+                    if let p = CLPlacemark(placemark: places?[0] as CLPlacemark) {
+                        var subThoroughfare:String = ""
+                        var thoroughfare:String = ""
+                        if p.subThoroughfare != nil {
+                            subThoroughfare = p.subThoroughfare
+                        }
+                        
+                        if p.thoroughfare != nil {
+                            thoroughfare = p.thoroughfare
+                        }
+                        
+                        annote = "\(subThoroughfare) \(thoroughfare)"
+                    }
+                }
+                
+                if annote == "" {
+                    annote = "added \(NSDate())"
+                }
+                println(annote)
+                var annot = MKPointAnnotation()
+                annot.coordinate = newCoord
+                annot.title = annote
+                self.map.addAnnotation(annot)
+            })
+            
+        }
+    }
+    
+    
+    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        var userLocation:CLLocation = locations[0] as CLLocation
+        var latitude = userLocation.coordinate.latitude
+        var longitude = userLocation.coordinate.longitude
+        var latDelta:CLLocationDegrees = 0.01
+        var logDelta:CLLocationDegrees = 0.01
+        var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, logDelta)
+        var coordinate = CLLocationCoordinate2DMake(latitude, longitude)
+        var region:MKCoordinateRegion = MKCoordinateRegionMake(coordinate, span)
+        self.map.setRegion(region, animated: true)
     }
 
     override func didReceiveMemoryWarning() {
